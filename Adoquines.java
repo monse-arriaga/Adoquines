f9f059d89accc3ed499b92380c8ab72b65a28548
monse-arriaga <monse_arriaga@ciencias.unam.mx>
2023-09-17 02:58:05 -0600

    Adoquines final

--- Adoquines.java
+++ Adoquines.java
@@ -108,10 +108,53 @@ public static String[][] adoquinamientoR(String[][] tablero, int random1, int ra
 }    
 
 
+    public static String[][] colorearTabla(String[][] tabla){
+        for(int i=0; i < horizontal-1; i++){
+        for(int j=0; j<vertical-1; j++){
+
+           if(tabla[i][j].equals("v") && tabla[i][j+1].equals("v") && tabla[i+1][j].equals("v") && (tabla[i+1][j+1] == "\033[36ma\u001B[0m"||tabla[i+1][j+1] =="\033[31me\u001B[0m")){
+                tabla[i][j] = "\u001B[32mb\u001B[0m";
+                tabla[i][j+1] = "\u001B[32mb\u001B[0m";
+                tabla[i+1][j] = "\u001B[32mb\u001B[0m";
+           }
+            
+   
+             if(tabla[i][j].equals("v") && tabla[i][j+1].equals("v") && (tabla[i+1][j]== "\033[36ma\u001B[0m"||tabla[i+1][j] =="\033[31me\u001B[0m") && tabla[i+1][j+1].equals("v")){
+                tabla[i][j] = "\u001B[35mc\u001B[0m";
+                tabla[i][j+1] = "\u001B[35mc\u001B[0m";
+                tabla[i+1][j+1] = "\u001B[35mc\u001B[0m";
+            }
+
+            if((tabla[i][j]== "\033[36ma\u001B[0m" || tabla[i][j]== "\033[31me\u001B[0m") && tabla[i][j+1].equals("v") && tabla[i+1][j].equals("v") && tabla[i+1][j+1].equals("v")){
+            tabla[i+1][j] = "\u001B[33md\u001B[0m";
+            tabla[i][j+1] = "\u001B[33md\u001B[0m";
+            tabla[i+1][j+1] = "\u001B[33md\u001B[0m";
+            }
+
+            if(tabla[i][j].equals("v") && (tabla[i][j+1]== "\033[36ma\u001B[0m" || tabla[i][j+1]== "\033[31me\u001B[0m") && tabla[i+1][j].equals("v") && tabla[i+1][j+1].equals("v")){
+            tabla[i+1][j] = "f";
+            tabla[i][j] = "f";
+            tabla[i+1][j+1] = "f";
+            }
+            
+
+        }
+    }
+    System.out.println();
+     for(int i=0; i < horizontal; i++){
+        for(int j=0; j<vertical; j++){
+            System.out.print(arreglo[i][j] + " ");
+        }
+        System.out.println();
+    }
+
+    System.out.println();
+    return tabla;
+    }
 
 
     public static void main(String[] args) {
-        adoquinamiento(crearTablero(3));
+        colorearTabla(adoquinamiento(crearTablero(3)));
         //System.out.println("\033[31m aaaa \033[37m");
     }
 }
